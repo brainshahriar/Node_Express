e6ae3bc64f1aca0592c4077ba27dc67197463fc0
shahriar <shahriar.mehedi@bjitgroup.com>
2022-02-04 20:00:56 +0600

    test

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 import express from "express";
-const express = require('express');
+const app = express()
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port http://localhost:${PORT}`)
+  })
