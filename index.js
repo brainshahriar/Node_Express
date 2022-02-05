7843030434321e3652575324e81a9283e7b0a2e7
shahriar <shahriar.mehedi@bjitgroup.com>
2022-02-05 22:57:41 +0600

    fs

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('hello');
-});
-
-
-app.get('/posts',(req,res)=>{
-    res.send('bye');
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status:'success',
+        result:tours.length,
+        data:{
+            tours
+        }
+    });
 });
 const port = 3000;
 app.listen(port,(req,res)=>{
