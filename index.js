7c46da794bd44e55cc5d241e8fe0509e14993e49
shahriar <shahriar.mehedi@bjitgroup.com>
2022-02-05 23:21:21 +0600

    post

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -14,6 +16,21 @@ app.get('/api/v1/tours',(req,res)=>{
         }
     });
 });
+
+app.get('/api/v1/tours/:id',(req,res)=>{
+    console.log(req.params);
+
+    const id=req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    res.status(200).json({
+        status:'success',
+        // result:tours.length,
+        data:{
+            tour
+        }
+    });
+});
+
 const port = 3000;
 app.listen(port,(req,res)=>{
     console.log(`App running in port ${port}...`);
