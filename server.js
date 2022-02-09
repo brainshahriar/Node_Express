939d87b327990c02ca7b650cc3aeb13719b4a19c
shahriar <shahriar.mehedi@bjitgroup.com>
2022-02-09 15:07:22 +0600

    mongo

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 const app = express();
-const dotenv = require('dotenv'); 
+const bodyparser = require('body-parser');
+const path = require('path');
+
 
 dotenv.config({path:'.env'});
-
 const PORT = process.env.PORT || 8080
-app.get('/',(req,res)=>{
-    res.send('crus');
-})
+
+//log request
+app.use(morgan('tiny'));
+//parse request 
+app.use(bodyparser.urlencoded({extended:true}))
+//view engine
+app.set("view engine","ejs")
+// app.set("views",path.resolve(__dirname,"views/ejs"))
+
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/image',express.static(path.resolve(__dirname,"assets/image")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+
+//load router
+
+app.use('/',require('./server/routes/router'))
+
 app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)})
